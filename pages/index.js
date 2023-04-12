adfccc9b181222b268d519b078e996b3cc5a652c
thewakelessone <wakeless.one@gmail.com>
2023-04-12 00:14:36 -0600

    refined layout a bit

--- pages/index.js
+++ pages/index.js
@@ -19,32 +19,41 @@ export default function Index() {
        <div className={styles.container}>
           <article className={styles.article}>
             <Image src={splashPic} alt="wakeless concophonic collective" />
-            <h2 className={styles.title}>Civilization is Rubble</h2>
-            <p>Capitalism is dying and the era of giant centralized power and wealth
-            hoarding corporations, banks, financial institutions, social and religious
-            organizations, and nation states is almost, mercifully, over. The fat rich
-            balding white dudes that have been hoarding the wealth of the world and
-            limiting the flow and access of information since the dawn of civilization
-            will not go down without a fight however so there are some ugly years to
-            come. But the war is already won as there is no power on heaven or earth
-            that is a match for the collective intelligence and will of the human race.
-            So order that case of Dom and polish your oyster tray but don't send out
-            the invitations just yet. There is still work to be done.
-            </p>
-            <p>If you are as excited and impatient as we are join the cause and foment
-            insurrection. Help us hasten the pace of the revolution or if you know of a
-            better way call us and we'll join you.</p>
-            <p>Wakeless is a conceptional framework designed to prepare us for the impending
-            transformation. A set of ideologies, methodologies, architectural schemes,
-            philosophical, social, cultural and political perspectives based on our best
-            educated guess of what the world will soon be like. These ideas are constantly
-            evolving and the more raw data, intelligent analysis, and unique perspective we
-             can build into the feedback loop the better these predictions will become. They
-             are designed primarily as a sort of template for creating or restructuring pretty
-             much any type of organization. If implemented well the resulting concoction
-             should be well suited for the new paradigm. We hope. One thing we know for
-             certain is that an entirely new approach is needed starting with a new
-             definition of success.</p>
+            <div className={styles.text}>
+              <p><b>Capitalism is dying.</b> And the era of all-powerful nation states, mega corporations
+              that control the weather, and central banks that print their own money is almost,
+              mercifully, over.</p>
+            </div>
+            <div className={styles.text}>
+              <p>The fat rich balding white men that have been hoarding the
+              wealth of the world and limiting the flow of information since the dawn of time
+              will not go down easy. So there are ugly years to come. But the war is as good as
+              won as there is no power on heaven or earth that is a match for the collective
+              intelligence of the human race.</p>
+            </div>
+            <div className={styles.text}>
+              <p>So order that case of Dom and polish your
+              oyster tray but don't send out the invitations just yet. There is still work to be done.
+              And If you are as excited and impatient as we are join the cause and foment
+              insurrection. Help us hasten the pace of the revolution or if you know of a
+              better way call us and we'll join you.</p>
+            </div>
+            <div className={styles.text}>
+              <p> . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . </p>
+            </div>
+            <div className={styles.text}>
+              <p><b>Wakeless is a conceptional framework designed to prepare us for the impending
+              transformation.</b> A set of ideologies, methodologies, architectural schemes,
+              philosophical, social, cultural and political perspectives based on our best
+              educated guess of what the world will soon be like. These ideas are constantly
+              evolving and the more raw data, intelligent analysis, and unique perspective we
+              can build into the feedback loop the better these predictions will become. They
+              are designed primarily as a sort of template for creating or restructuring pretty
+              much any type of organization. If implemented well the resulting concoction
+              should be well suited for the new paradigm. We hope. One thing we know for
+              certain is that an entirely new approach is needed starting with a new
+              definition of success.</p>
+            </div>
           </article>
           <div className={styles.padding}> </div>
           <article className={styles.article}>
